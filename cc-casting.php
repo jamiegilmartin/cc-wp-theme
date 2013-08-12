f7667a6d91ecb25cf9cf44babc018b268179eb35
jamiegilmartin <jamiegilmartin@gmail.com>
2013-08-12 13:39:02 -0400

    casting fix

--- cc-casting.php
+++ cc-casting.php
@@ -6,8 +6,9 @@ Template Name: Casting
 get_header(); ?>
 <div id="content" class="cc-casting">
 	<section class="content">
-		<p>Casting for the United Bamboo 2014 Cat Calendar has been closed.  <a href="http://unitedbamboo.us6.list-manage1.com/subscribe?u=6b9528ffebce1155e342ec488&id=3267b6a7cb" target="_blank">Subscribe to our mailing</a> list and be the first to know when we announce next year's casting!</p>
-		
+		<div class="callout">
+			<p>Casting for the United Bamboo 2014 Cat Calendar has been closed.  <a href="http://unitedbamboo.us6.list-manage1.com/subscribe?u=6b9528ffebce1155e342ec488&id=3267b6a7cb" target="_blank">Subscribe to our mailing</a> list and be the first to know when we announce next year's casting!</p>
+		</div>
 		<iframe width="100%" height="100%" src="http://www.youtube.com/embed/oR-AjeBe3QM?autoplay=1&rel=0" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 
 	</section>
